11b8c721047d0f7950805cddd27bd2369c8b1c2f
sb <sbenthall@gmail.com>
2018-08-10 16:14:30 -0400

    adding go mod and org-ref

--- init.el
+++ init.el
@@ -42,4 +42,26 @@
 (add-hook 'scala-mode-hook
             '(lambda ()
            (scala-mode-feature-electric-mode)
-               ))
+           ))
+
+
+(require 'package)
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                    (not (gnutls-available-p))))
+       (proto (if no-ssl "http" "https")))
+  ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
+  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
+  ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
+  (when (< emacs-major-version 24)
+    ;; For important compatibility libraries like cl-lib
+    (add-to-list 'package-archives '("gnu" . (concat proto "://elpa.gnu.org/packages/")))))
+(package-initialize)
+
+(require 'org-ref)
+
+
+(add-to-list 'load-path "~/.emacs.d/go-mode.el/")
+(require 'go-mode)
+
+;;(autoload 'go-mode "go-mode.el" nil t)
+(add-to-list 'auto-mode-alist '("\\.go\\'" . go-mode))
